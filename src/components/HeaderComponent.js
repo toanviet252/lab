1b0a9c601ae1941863a2661d55d931b6ba4f30c6
Toan Viet <toankvfx17375@funix.edu.vn>
2022-07-15 22:16:15 +0700

    lab06_1

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -41,12 +41,18 @@ class Header extends Component {
                     <NavLink className="nav-link" to="/home">
                       <span className="fa fa-home fa-lg">Home</span>
                     </NavLink>
+                  </NavItem>
+                  <NavItem>
                     <NavLink className="nav-link" to="/aboutus">
                       <span className="fa fa-info fa-lg">About us</span>
                     </NavLink>
+                  </NavItem>
+                  <NavItem>
                     <NavLink className="nav-link" to="/menu">
                       <span className="fa fa-list fa-lg">Menu</span>
                     </NavLink>
+                  </NavItem>
+                  <NavItem>
                     <NavLink className="nav-link" to="/contactus">
                       <span className="fa fa-address-card fa-lg">
                         Contact us
